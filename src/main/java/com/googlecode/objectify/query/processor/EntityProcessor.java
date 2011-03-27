7325454b78a45e8160412bcfb9fcb2fd9477d1f0
Brendan Doherty <brendan@propertysimplified.com>
2011-03-27 20:11:28 +1300

    add -SNAPSHOT to version

---
+++ src/main/java/com/googlecode/objectify/query/processor/EntityProcessor.java
@@ -0,0 +1,363 @@
+package com.googlecode.objectify.query.processor;
+
+import static javax.lang.model.SourceVersion.RELEASE_6;
+
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.VariableElement;
+import javax.lang.model.type.DeclaredType;
+import javax.lang.model.util.ElementFilter;
+import javax.tools.Diagnostic.Kind;
+
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Indexed;
+import com.googlecode.objectify.annotation.NotSaved;
+import com.googlecode.objectify.annotation.Parent;
+import com.googlecode.objectify.annotation.Unindexed;
+import com.googlecode.objectify.query.annotation.List;
+import com.googlecode.objectify.query.annotation.List.KeyType;
+
+@SupportedAnnotationTypes("com.googlecode.objectify.annotation.Entity")
+@SupportedSourceVersion(RELEASE_6)
+public class EntityProcessor extends AbstractProcessor {
+
+  private ProcessingEnvironment env;
+
+  @Override
+  public synchronized void init(ProcessingEnvironment env) {
+    super.init(env);
+
+    this.env = env;
+  }
+
+  @Override
+  public boolean process(Set<? extends TypeElement> annotations,
+      RoundEnvironment roundEnv) {
+
+    if (!roundEnv.processingOver()) {
+
+      for (TypeElement currAnnotation : annotations) {
+
+        if (currAnnotation.getQualifiedName().contentEquals(
+            Entity.class.getName())) {
+
+          for (Element entity : roundEnv.getElementsAnnotatedWith(currAnnotation)) {
+            this.processEntity(entity);
+          }
+
+        }
+      }
+
+    }
+
+    return true;
+  }
+
+  private void processEntity(Element entityElement) {
+    PrintWriter out = null;
+
+    String entityPackageName = env.getElementUtils().getPackageOf(entityElement).getQualifiedName().toString();
+    String entityName = entityElement.getSimpleName().toString();
+    String queryName = entityName + "Query";
+    String queryPackageName = entityPackageName.replaceAll("\\.shared\\.",
+        ".server.");
+    String queryQName = queryPackageName + "." + queryName;
+
+    Unindexed unindexedClass = entityElement.getAnnotation(Unindexed.class);
+
+    VariableElement parentField = null;
+
+    try {
+      out = new PrintWriter(
+          new BufferedWriter(this.env.getFiler().createSourceFile(queryQName,
+              entityElement).openWriter()));
+
+      // this.env.getMessager().printMessage(Kind.ERROR, entityName);
+
+      out.println("package " + queryPackageName + ";");
+      out.println();
+      out.println("import java.util.ArrayList;");
+      out.println("import java.util.Map;");
+      out.println();
+      out.println("import com.google.appengine.api.datastore.Cursor;");
+      out.println("import com.google.appengine.api.datastore.QueryResultIterator;");
+      out.println("import com.googlecode.objectify.Key;");
+      out.println("import com.googlecode.objectify.Objectify;");
+      out.println("import com.googlecode.objectify.ObjectifyOpts;");
+      out.println("import com.googlecode.objectify.ObjectifyService;");
+      out.println("import com.googlecode.objectify.Query;");
+      try {
+        // objectify 3.x
+        Class.forName("com.googlecode.objectify.util.QueryWrapper");
+        out.println("import com.googlecode.objectify.util.QueryWrapper;");
+      } catch (ClassNotFoundException e) {
+        try {
+          // objectify 2.x
+          Class.forName("com.googlecode.objectify.helper.QueryWrapper");
+          out.println("import com.googlecode.objectify.helper.QueryWrapper;");
+        } catch (ClassNotFoundException e1) {
+          env.getMessager().printMessage(Kind.ERROR,
+              "Could not find QueryWrapper class");
+        }
+      }
+      out.println("import com.googlecode.objectify.query.shared.ListPage;");
+      out.println("import " + entityPackageName + "." + entityName + ";");
+      out.println();
+      out.println("public class " + queryName + " extends QueryWrapper<"
+          + entityName + "> { ");
+      out.println();
+      out.println("  private final Query<" + entityName + "> query;");
+      out.println("  private Objectify lazyOfy;");
+      out.println();
+      out.println("  public " + queryName + "(Query<" + entityName
+          + "> query) {");
+      out.println("    super(query);");
+      out.println("    this.query = query;");
+      out.println("  }");
+      out.println();
+
+      for (VariableElement fieldElement : ElementFilter.fieldsIn(env.getElementUtils().getAllMembers(
+          (TypeElement) entityElement))) {
+
+        Unindexed unindexedField = fieldElement.getAnnotation(Unindexed.class);
+        // Transient transientField =
+        // fieldElement.getAnnotation(Transient.class);
+        NotSaved notSavedField = fieldElement.getAnnotation(NotSaved.class);
+        Indexed indexedField = fieldElement.getAnnotation(Indexed.class);
+
+        Parent parent = fieldElement.getAnnotation(Parent.class);
+        if (parentField == null && parent != null) {
+          parentField = fieldElement;
+        }
+
+        if (unindexedField != null && unindexedField.value().length == 0) {
+          // @Unindexed field without If... parameter
+        } else if (notSavedField != null) {
+          // @NotSaved field
+          // } else if(transientField != null) {
+          // // @Transient field
+        } else if (unindexedClass != null && indexedField == null) {
+          // @Unindexed class and field is not @Indexed
+        } else {
+          String fieldName = fieldElement.getSimpleName().toString();
+          String fieldType = env.getTypeUtils().asMemberOf(
+              (DeclaredType) entityElement.asType(), fieldElement).toString();
+
+          out.println("  public " + queryName + " filterBy"
+              + fieldName.substring(0, 1).toUpperCase()
+              + fieldName.substring(1) + "(" + fieldType + " " + fieldName
+              + ") {");
+          if (parent != null) {
+            out.println("    this.query.ancestor(" + fieldName + ");");
+            out.println("    return this;");
+          } else {
+            out.println("    this.query.filter(\"" + fieldName + "\", "
+                + fieldName + ");");
+            out.println("    return this;");
+          }
+          out.println("  }");
+          out.println();
+
+          if (parent == null) {
+            out.println("  public " + queryName + " filterBy"
+                + fieldName.substring(0, 1).toUpperCase()
+                + fieldName.substring(1) + "(String operation, Object value) {");
+            out.println("    this.query.filter(\"" + fieldName
+                + " \" + operation, value);");
+            out.println("    return this;");
+            out.println("  }");
+            out.println();
+          }
+        }
+      }
+
+      out.println("  public ListPage<" + entityName
+          + "> list(String cursor, int pageSize) {");
+      out.println("    if (cursor != null) {");
+      out.println("      this.query.startCursor(Cursor.fromWebSafeString(cursor));");
+      out.println("    }");
+      out.println("    QueryResultIterator<" + entityName
+          + "> iterator = this.query.iterator();");
+      out.println("    boolean more = false;");
+      out.println("    ArrayList<" + entityName + "> list = new ArrayList<"
+          + entityName + ">();");
+      out.println("    for (int i = 0; i < pageSize && (more = iterator.hasNext()); i++) {");
+      out.println("      list.add(iterator.next());");
+      out.println("    }");
+      out.println("    return new ListPage<" + entityName
+          + ">(list, iterator.getCursor().toWebSafeString(), more);");
+      out.println("  }");
+      out.println();
+
+      out.println("  public ListPage<Key<" + entityName
+          + ">> listKeys(String cursor, int pageSize) {");
+      out.println("    if (cursor != null) {");
+      out.println("      this.query.startCursor(Cursor.fromWebSafeString(cursor));");
+      out.println("    }");
+
+      out.println("    QueryResultIterator<Key<" + entityName
+          + ">> iterator = this.query.fetchKeys().iterator();");
+      out.println("    boolean more = false;");
+      out.println("    ArrayList<Key<" + entityName
+          + ">> list = new ArrayList<Key<" + entityName + ">>();");
+      out.println("    for (int i = 0; i < pageSize && (more = iterator.hasNext()); i++) {");
+      out.println("      list.add(iterator.next());");
+      out.println("    }");
+      out.println("    return new ListPage<Key<" + entityName
+          + ">>(list, iterator.getCursor().toWebSafeString(), more);");
+      out.println("  }");
+
+      if (parentField != null) {
+        String fieldType = env.getTypeUtils().asMemberOf(
+            (DeclaredType) entityElement.asType(), parentField).toString();
+        fieldType = fieldType.substring(fieldType.indexOf('<') + 1,
+            fieldType.lastIndexOf('>'));
+
+        out.println();
+        out.println("  public ListPage<" + fieldType
+            + "> listParents(String cursor, int pageSize) {");
+        out.println();
+        out.println("    if (cursor != null) {");
+        out.println("      this.query.startCursor(Cursor.fromWebSafeString(cursor));");
+        out.println("    }");
+        out.println("    QueryResultIterator<Key<" + entityName
+            + ">> iterator = this.query.fetchKeys().iterator();");
+        out.println();
+        out.println("    boolean more = false;");
+        out.println("    ArrayList<Long> idList = new ArrayList<Long>();");
+        out.println("    for (int i = 0; i < pageSize && (more = iterator.hasNext()); i++) {");
+        out.println("      idList.add(iterator.next().getParent().getId());");
+        out.println("    }");
+        out.println();
+        out.println("    Map<Long, " + fieldType + "> list = ofy().get("
+            + fieldType + ".class, idList);");
+        out.println("    return new ListPage<" + fieldType + ">(new ArrayList<"
+            + fieldType + ">(");
+        out.println("      list.values()), iterator.getCursor().toWebSafeString(), more);");
+        out.println("  }");
+        out.println();
+        out.println("  public ListPage<Key<" + fieldType
+            + ">> listParentKeys(String cursor, int pageSize) {");
+        out.println();
+        out.println("    if (cursor != null) {");
+        out.println("      this.query.startCursor(Cursor.fromWebSafeString(cursor));");
+        out.println("    }");
+        out.println("    QueryResultIterator<Key<" + entityName
+            + ">> iterator = this.query.fetchKeys().iterator();");
+        out.println();
+        out.println("    boolean more = false;");
+        out.println("    ArrayList<Key<" + fieldType
+            + ">> idList = new ArrayList<Key<" + fieldType + ">>();");
+        out.println("    for (int i = 0; i < pageSize && (more = iterator.hasNext()); i++) {");
+        out.println("      idList.add(new Key<" + fieldType + ">(" + fieldType
+            + ".class, iterator.next().getParent().getId()));");
+        out.println("    }");
+        out.println();
+        out.println("    return new ListPage<Key<" + fieldType
+            + ">>(idList, iterator.getCursor().toWebSafeString(), more);");
+        out.println("  }");
+        out.println();
+      }
+
+      for (ExecutableElement methodElement : ElementFilter.methodsIn(env.getElementUtils().getAllMembers(
+          (TypeElement) entityElement))) {
+
+        if (methodElement.getParameters().size() == 0) {
+          List listField = methodElement.getAnnotation(List.class);
+
+          if (listField != null) {
+
+            String returnType = methodElement.getReturnType().toString();
+            returnType = returnType.substring(returnType.indexOf('<') + 1,
+                returnType.lastIndexOf('>'));
+            String idType;
+            String idAccessor;
+            if (listField.keyType() == KeyType.Id) {
+              idType = "Long";
+              idAccessor = "getId()";
+            } else {
+              idType = "String";
+              idAccessor = "getName()";
+            }
+
+            out.println("  public ListPage<Key<" + returnType + ">> list"
+                + listField.singularName()
+                + "Keys(String cursor, int pageSize) {");
+            out.println();
+            out.println("    if (cursor != null) {");
+            out.println("      this.query.startCursor(Cursor.fromWebSafeString(cursor));");
+            out.println("    }");
+            out.println("    QueryResultIterator<" + entityName
+                + "> iterator = query.iterator();");
+            out.println();
+            out.println("    boolean more = false;");
+            out.println("    ArrayList<Key<" + returnType
+                + ">> idList = new ArrayList<Key<" + returnType + ">>();");
+            out.println("    for (int i = 0; i < pageSize && (more = iterator.hasNext()); i++) {");
+            out.println("      idList.add(iterator.next()."
+                + methodElement.getSimpleName().toString() + "());");
+            out.println("    }");
+            out.println();
+            out.println("    return new ListPage<Key<" + returnType
+                + ">>(idList, iterator.getCursor()");
+            out.println("        .toWebSafeString(), more);");
+            out.println("  }");
+            out.println();
+            out.println("  public ListPage<" + returnType + "> list"
+                + listField.pluralName() + "(String cursor, int pageSize) {");
+            out.println("    if (cursor != null) {");
+            out.println("      this.query.startCursor(Cursor.fromWebSafeString(cursor));");
+            out.println("    }");
+            out.println("    QueryResultIterator<" + entityName
+                + "> iterator = query.iterator();");
+            out.println();
+            out.println("    boolean more = false;");
+            out.println("    ArrayList<" + idType + "> idList = new ArrayList<"
+                + idType + ">();");
+            out.println("    for (int i = 0; i < pageSize && (more = iterator.hasNext()); i++) {");
+            out.println("      idList.add(iterator.next()."
+                + methodElement.getSimpleName().toString() + "()." + idAccessor
+                + ");");
+            out.println("    }");
+            out.println();
+            out.println("    Map<" + idType + ", " + returnType + "> list =");
+            out.println("      ofy().get(" + returnType + ".class, idList);");
+            out.println("    return new ListPage<" + returnType
+                + ">(new ArrayList<" + returnType + ">(");
+            out.println("      list.values()), iterator.getCursor().toWebSafeString(), more);");
+            out.println("  }");
+          }
+        }
+      }
+      out.println();
+      out.println("  protected Objectify ofy() {");
+      out.println("    if (this.lazyOfy == null) {");
+      out.println("      ObjectifyOpts opts = new ObjectifyOpts().setSessionCache(true);");
+      out.println("      this.lazyOfy = ObjectifyService.factory().begin(opts);");
+      out.println("    }");
+      out.println("    return this.lazyOfy;");
+      out.println("  }");
+      out.println();
+
+      out.println("}");
+
+    } catch (java.io.IOException e) {
+      this.env.getMessager().printMessage(Kind.ERROR, e.toString());
+      throw new RuntimeException(e);
+    } finally {
+      if (out != null) {
+        out.close();
+      }
+    }
+  }
+}
---
+++ src/main/java/com/googlecode/objectify/query/shared/ListPage.java
@@ -0,0 +1,153 @@
+/*
+ * Brendan Doherty, trading as PropertySimplified ("COMPANY") CONFIDENTIAL
+ * Unpublished Copyright (c) 2010 PropertySimplified, All Rights Reserved.
+ * 
+ * NOTICE: All information contained herein is, and remains the property of
+ * COMPANY. The intellectual and technical concepts contained herein are
+ * proprietary to COMPANY and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material is
+ * strictly forbidden unless prior written permission is obtained from COMPANY.
+ * Access to the source code contained herein is hereby forbidden to anyone
+ * except current COMPANY employees, managers or contractors who have executed
+ * Confidentiality and Non-disclosure agreements explicitly covering such
+ * access.
+ * 
+ * The copyright notice above does not evidence any actual or intended
+ * publication or disclosure of this source code, which includes information
+ * that is confidential and/or proprietary, and is a trade secret, of COMPANY.
+ * ANY REPRODUCTION, MODIFICATION, DISTRIBUTION, PUBLIC PERFORMANCE, OR PUBLIC
+ * DISPLAY OF OR THROUGH USE OF THIS SOURCE CODE WITHOUT THE EXPRESS WRITTEN
+ * CONSENT OF COMPANY IS STRICTLY PROHIBITED, AND IN VIOLATION OF APPLICABLE
+ * LAWS AND INTERNATIONAL TREATIES. THE RECEIPT OR POSSESSION OF THIS SOURCE
+ * CODE AND/OR RELATED INFORMATION DOES NOT CONVEY OR IMPLY ANY RIGHTS TO
+ * REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS, OR TO MANUFACTURE, USE, OR
+ * SELL ANYTHING THAT IT MAY DESCRIBE, IN WHOLE OR IN PART.
+ */
+
+package com.googlecode.objectify.query.shared;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.RandomAccess;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+/**
+ * A "page" of results returned from an objectify-query generated query object.
+ * <p/>
+ * If there are more results still available to be returned from the query, 
+ * the <code>more</code> operation will return true.  
+ * 
+ * Use the <code>getCursor</code> operation to retrieve the pointer to the 
+ * last location of the query.   
+ * 
+ * ListPage is GWT-RPC friendly.
+ * 
+ * @author Brendan Doherty
+ *
+ * @param <Entity_> 
+ */
+public class ListPage<Entity_> implements Iterable<Entity_>, RandomAccess, IsSerializable {
+
+	private ArrayList<Entity_> list;
+	private String cursor;
+	private boolean more;
+
+	ListPage() {
+	}
+
+	public ListPage(ArrayList<Entity_> list, String cursor, boolean more) {
+		assert list != null;
+		this.list = list;
+		this.cursor = cursor;
+		this.more = more;
+	}
+
+	public boolean contains(Entity_ entity) {
+		return this.list.contains(entity);		
+	}
+
+	public Entity_ get(int index) {
+		return this.list.get(index);
+	}
+
+	public int indexOf(Entity_ entity) {
+		return this.list.indexOf(entity);
+	}
+
+	public boolean isEmpty() {
+		return this.list.isEmpty();
+	}
+
+	public Iterator<Entity_> iterator() {
+		return this.list.iterator();
+	}
+
+	public int lastIndexOf(Entity_ entity) {
+		return this.list.lastIndexOf(entity);
+	}
+
+	public ListIterator<Entity_> listIterator() {
+		return this.list.listIterator();
+	}
+
+	public int size() {
+		return this.list.size();
+	}
+
+	public List<Entity_> subList(int arg0, int arg1) {
+		return this.list.subList(arg0, arg1);
+	}
+	
+	public String getCursor() {
+		return this.cursor;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 *   <code>true</code> if there are more results still available to be returned from the query.<br/>
+	 *   <code>false</code> if this page is the last page.
+	 * 
+	 */
+	public boolean more() {
+		return this.more;
+	}
+		
+    @Override
+    public int hashCode() {
+        int hashCode = 23;
+        hashCode = (hashCode * 37) + getClass().hashCode();
+        hashCode = (hashCode * 37) + (this.list == null ? 1 : this.list.hashCode());
+        hashCode = (hashCode * 37) + (this.cursor == null ? 1 : this.cursor.hashCode());
+        hashCode = (hashCode * 37) + Boolean.valueOf(this.more).hashCode();
+        return hashCode;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other != null && other.getClass().equals(this.getClass())) {
+        	ListPage<?> o = (ListPage<?>) other;
+            return true
+                && ((o.list == null && this.list == null) || (o.list != null && o.list.equals(this.list)))
+                && ((o.cursor == null && this.cursor == null) || (o.cursor != null && o.cursor.equals(this.cursor)))
+                && o.more == this.more
+               ;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "ListPage["
+            + this.list
+            + ","
+            + this.cursor
+            + ","
+            + this.more
+            + "]";
+    }
+}
